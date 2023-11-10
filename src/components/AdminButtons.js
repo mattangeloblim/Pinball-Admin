1048eb46fb8723bbb048bd8518f7e884ecd915c4
mattangeloblim <matt.lim@breddas.com.ph>
2023-11-10 15:13:33 +0800

    commit changes

--- src/components/AdminButtons.js
+++ src/components/AdminButtons.js
@@ -1,9 +1,51 @@
 /* eslint-disable */
 import React, { useEffect, useState } from "react";
+import "./style.css"
 
 import { Button } from "@mui/material";
 
 import useAdminContext from "../context/AdminContext";
+
+const Modal = ({ color, isOpen, onClose, onConfirm }) => {
+
+  const colorNames = {
+    "#ED3130": "Red",
+    "#276ADD": "Blue",
+    "#F4FF63": "Yellow",
+    "#56DE33": "Green",
+    "#FFD700": "Gold",
+    "#9A3FBC": "Purple",
+    "#F08F40": "Orange",
+    "#DC63D0": "Pink",
+    "#33C5ED": "Cyan",
+  };
+
+  const colorName = colorNames[color] || color;
+
+  return (
+    <div className={`fixed top-0 left-0 w-full h-full flex items-center justify-center ${isOpen ? '' : 'hidden'}`}>
+      <div className="absolute w-full h-full bg-gray-800 opacity-50"></div>
+      <div className="z-10 w-1/4 bg-white rounded p-8 text-center">
+        <p className="mb-4">Please confirm that the winning color is <strong>{colorName}</strong>.</p>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+          onClick={() => {
+            onConfirm();
+            onClose();
+          }}
+        >
+          Confirm
+        </button>
+        <button
+          className="ml-2 bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600"
+          onClick={onClose}
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
 
 function AdminButtons() {
   const {
@@ -17,30 +59,50 @@ function AdminButtons() {
 
   const [isClicked, setIsClicked] = useState(false);
   const [clickedIndex, setClickedIndex] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isBlinking, setIsBlinking] = useState(false);
   // const [currentGameId, setCurrentGameId] = useState(null);
 
   //disable other buttons for 5 sec after button click
   const handleButtonClick = (color, index) => {
     if (!isClicked) {
       buttonVisibilityHandler(color, index);
-      handleGenerateResult(index);
       setIsClicked(true);
       setClickedIndex(index);
+      openModal();
 
-      setTimeout(() => {
-        setIsClicked(false);
-        setClickedIndex(null);
-        setWinningColor("");
-      }, 5000);
     }
   };
 
-  // useEffect(() => {
-  //   setCurrentGameId(gameId);
-  // }, [gameId]);
+  const openModal = () => {
+    // setWinningColor(color)
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    // Reset state after closing the modal
+    setIsClicked(false);
+    setClickedIndex(null);
+    // setWinningColor("")
+  };
+
+  const confirmAndGenerateResult = () => {
+    const color = colorHex[clickedIndex];
+    // Additional logic if needed before generating result
+    handleGenerateResult(clickedIndex);
+    setWinningColor(color)
+    // Close the modal
+    setIsModalOpen(false);
+    setIsBlinking(true); 
+    setTimeout(() => {
+      setIsBlinking(false);
+    }, 5000);
+  };
+
   return (
     <div className="border-2 border-blue-600 flex flex-col justify-center">
-      <h1 className="m-6 uppercase text-3xl font-bold text-center">
+      <h1 className={`m-6 uppercase text-3xl font-bold text-center ${isBlinking ? 'blink-animation' : ''}`}>
         game id: <span>{gameId}</span>
       </h1>
       <h1 className="m-6 uppercase text-lg font-bold text-start">
@@ -63,7 +125,7 @@ function AdminButtons() {
           ></Button>
         ))}
       </div>
-      <div className="flex justify-center items-center gap-4 m-6 ">
+      <div className={`flex justify-center items-center gap-4 m-6 ${isBlinking ? 'blink-animation' : ''}`}>
         <h2 className="uppercase font-bold text-md text-center">
           winning color:
         </h2>
@@ -72,6 +134,7 @@ function AdminButtons() {
           style={{ backgroundColor: winningColor }}
         ></div>
       </div>
+      <Modal color={winningColor} isOpen={isModalOpen} onClose={closeModal} onConfirm={confirmAndGenerateResult} />
     </div>
   );
 }
