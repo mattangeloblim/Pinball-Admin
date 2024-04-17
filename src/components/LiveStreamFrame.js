d5cd9ec8a5a9de59d410f6d8290450d909ebd3d8
Kyle Dominic Mendoza <kylemendoza67@gmail.com>
2024-04-17 18:40:12 +0800

    stream url updated

--- src/components/LiveStreamFrame.js
+++ src/components/LiveStreamFrame.js
@@ -9,7 +9,8 @@ function LiveStreamFrame() {
         allow="fullscreen"
         // width="1280"
         // height="720"
-        src="https://demo.nanocosmos.de/nanoplayer/embed/1.3.3/nanoplayer.html?group.id=8c017f99-1128-44c1-af13-e20d7118e303&options.adaption.rule=deviationOfMean2&startIndex=0&playback.latencyControlMode=classic"></iframe>
+        src="https://demo.nanocosmos.de/nanoplayer/embed/1.3.3/nanoplayer.html?group.id=f39ebb0e-54e9-426f-908f-540925d2a58b&options.adaption.rule=deviationOfMean2&startIndex=0&playback.latencyControlMode=classic"
+      ></iframe>
     </div>
   );
 }
